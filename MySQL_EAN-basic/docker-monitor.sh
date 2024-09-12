ac43c1f64357d6d6dbdf9e5ebe56c1e1f1cd54d9
jlh7 <j.lh.t@outlook.com>
2024-09-12 15:39:12 +0000

    Update

--- MySQL_EAN-basic/docker-monitor.sh
+++
@@ -1,8 +0,0 @@
-lsContainer="echo $ docker ps -a && docker ps -a && echo"
-lsImages="echo $ docker images && docker images && echo"
-lsVolumes="echo $ docker volume ls && docker volume ls && echo"
-lsNetworks="echo $ docker network ls && docker network ls && echo"
-lsCompose="echo $ docker compose ls && docker compose ls && echo"
-lsFile="echo $ ls /home/docker/mean/ && ls /home/docker/mean/ && echo"
-
-watch -n 1 "$lsContainer && $lsImages && $lsVolumes && $lsNetworks && $lsCompose && $lsFile"
